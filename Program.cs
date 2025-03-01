538ec7690e370c04505d8414c5891b9a265cd010
Vekolyram <vekooolyyyram@outlook.com>
2025-03-01 21:44:27 +0800

    aa

--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ namespace BAAsciiStart
     {
         private static void Main(string[] args)
         {
-            Color c = Color.FromArgb();
-            Console.WriteLine($"H: {red.h:0.##}°, S: {red.s:0.##}, V: {red.v:0.##}");
+            //ConsoleColor.GetName
+            //Color c = Color.FromArgb();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            //Console.WriteLine($"H: {red.h:0.##}°, S: {red.s:0.##}, V: {red.v:0.##}");
         }
     }
 }
