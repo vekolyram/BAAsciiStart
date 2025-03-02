3c5389c7f5ca0938a68c6e8f8db88c9fc207e56e
Vekolyram <vekooolyyyram@outlook.com>
2025-03-02 11:51:22 +0800

    a

--- Program.cs
+++ Program.cs
@@ -1,5 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+
+using System.Drawing;
+
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+
+/*
+      _____                    _____                    _____                    _____                    _____
+     |\    \                  /\    \                  /\    \                  /\    \                  /\    \
+     |:\____\                /::\____\                /::\____\                /::\____\                /::\    \
+     |::|   |               /:::/    /               /:::/    /               /:::/    /               /::::\    \
+     |::|   |              /:::/    /               /:::/    /               /:::/    /               /::::::\    \
+     |::|   |             /:::/    /               /:::/    /               /:::/    /               /:::/\:::\    \
+     |::|   |            /:::/    /               /:::/    /               /:::/____/               /:::/__\:::\    \
+     |::|   |           /:::/    /               /:::/    /               /::::\    \              /::::\   \:::\    \
+     |::|___|______    /:::/    /      _____    /:::/    /      _____    /::::::\____\________    /::::::\   \:::\    \
+     /::::::::\    \  /:::/____/      /\    \  /:::/____/      /\    \  /:::/\:::::::::::\    \  /:::/\:::\   \:::\    \
+    /::::::::::\____\|:::|    /      /::\____\|:::|    /      /::\____\/:::/  |:::::::::::\____\/:::/  \:::\   \:::\____\
+   /:::/~~~~/~~      |:::|____\     /:::/    /|:::|____\     /:::/    /\::/   |::|~~~|~~~~~     \::/    \:::\  /:::/    /
+  /:::/    /          \:::\    \   /:::/    /  \:::\    \   /:::/    /  \/____|::|   |           \/____/ \:::\/:::/    /
+ /:::/    /            \:::\    \ /:::/    /    \:::\    \ /:::/    /         |::|   |                    \::::::/    /
+/:::/    /              \:::\    /:::/    /      \:::\    /:::/    /          |::|   |                     \::::/    /
+\::/    /                \:::\__/:::/    /        \:::\__/:::/    /           |::|   |                     /:::/    /
+ \/____/                  \::::::::/    /          \::::::::/    /            |::|   |                    /:::/    /
+                           \::::::/    /            \::::::/    /             |::|   |                   /:::/    /
+                            \::::/    /              \::::/    /              \::|   |                  /:::/    /
+                             \::/____/                \::/____/                \:|   |                  \::/    /
+                              ~~                       ~~                       \|___|                   \/____/
+
+*/
 
 namespace BAAsciiStart
 
@@ -10,10 +42,16 @@ namespace BAAsciiStart
         {
             //ConsoleColor.GetName
             Color c = Color.FromArgb(0xAACCDD);
-            Color h = ColorConverter.RgbNoHsB
+            Color h = ColorConverter.RgbNoHsB(c);
             //Console.ForegroundColor = ConsoleColor.Gray;
             //Console.WriteLine($"H: {red.h:0.##}°, S: {red.s:0.##}, V: {red.v:0.##}");
+            Console.WriteLine($"{c.GetHue()};{c.GetSaturation()}");
+            Console.WriteLine($"{h.GetHue()};{h.GetSaturation()}");
             Console.WriteLine($"\x1b[38;2;{c.R};{c.G};{c.B}maaa");
+            Console.WriteLine($"\x1b[38;2;{h.R};{h.G};{h.B}maaa");
+            var calculator = new CharCoverageCalculator("JetBrains Mono", 12, 1.2f);
+            calculator.test();
+            var a = new SortedList<float, char>() { { 1f, 'a' } };
         }
     }
 }
---
+++ fontTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+
+namespace BAAsciiStart
+{
+    public class CharCoverageCalculator
+    {
+        private readonly Font _font;
+        private readonly int _cellWidth;
+        private readonly int _cellHeight;
+
+        public CharCoverageCalculator(string fontName, float fontSize, float lineHeightMultiplier)
+        {
+            _font = new Font(fontName, fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
+            using (var tempBmp = new Bitmap(1, 1))
+            using (var g = Graphics.FromImage(tempBmp))
+            {
+                g.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+                _cellWidth = (int)Math.Ceiling(g.MeasureString(" ", _font).Width); // 空格字符宽度
+            }
+            _cellHeight = (int)Math.Round(fontSize * lineHeightMultiplier);
+        }
+
+        public float CalculateCoverage(char c)
+        {
+            var bmp = new Bitmap(_cellWidth, _cellHeight);
+            var g = Graphics.FromImage(bmp);
+            g.Clear(Color.Black);
+            g.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+            var format = new StringFormat
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+            g.DrawString(
+                c.ToString(),
+                _font,
+                Brushes.White,
+                new RectangleF(0, 0, _cellWidth, _cellHeight),
+                format
+            );
+            int foregroundPixels = 0;
+            var data = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                ImageLockMode.ReadOnly,
+                PixelFormat.Format32bppArgb
+            );
+            unsafe
+            {
+                byte* ptr = (byte*)data.Scan0;
+                for (int y = 0; y < data.Height; y++)
+                {
+                    for (int x = 0; x < data.Width; x++)
+                    {
+                        if (ptr[y * data.Stride + x * 4] > 0)
+                            foregroundPixels++;
+                    }
+                }
+            }
+            bmp.UnlockBits(data);
+            return (float)foregroundPixels / (_cellWidth * _cellHeight);
+        }
+
+        public SortedList<float, char> test()
+        {
+            var calculator = new CharCoverageCalculator("JetBrains Mono", 12, 1.2f);
+            var sortedList = new SortedList<float, char>();
+            foreach (char ch in " !@#$%^&-_*~,`{[:;'\"\\|?/▁▂▃▄▅▆▇█")
+            {
+                float hashCoverage = CalculateCoverage(ch);
+                hashCoverage = (float)Math.Round(hashCoverage, 2);
+                try
+                {
+                    sortedList.Add(hashCoverage, ch);
+                }
+                catch (ArgumentException)
+                {
+                    try
+                    {
+                        hashCoverage += 0.1f;
+                        sortedList.Add(hashCoverage, ch);
+                    }
+                    catch { }
+                }
+            }
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                Console.WriteLine($"{sortedList.Keys[i]} 的覆盖率: {sortedList[sortedList.Keys[i]]}");
+            }
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                Console.WriteLine($"{sortedList.Keys[i]}:'{sortedList[sortedList.Keys[i]]})',");
+            }
+            return sortedList;
+        }
+    }
+}
